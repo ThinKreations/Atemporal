ec30bacd4b03d805eaaa5d265d2653c28ecb493f
ThinKreations <jnapolesm1800@alumno.ipn.mx>
2022-08-17 18:33:13 -0500

    a ver

--- components/MainHead.jsx
+++ components/MainHead.jsx
@@ -14,7 +14,7 @@ return(
         <link rel="preconnect" href="https://fonts.gstatic.com" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet"/>
 
         <link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,300;1,300&display=swap" rel="stylesheet"></link>
