70ea61935d684138f23acba3dc8823b39cb3ec43
ThinKreations <jnapolesm1800@alumno.ipn.mx>
2022-08-17 18:36:01 -0500

    a ver

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
-import logo from '../src/atempologo.png'
+
 import btn from '../src/lateral.png'
 import MainHead from '../components/MainHead'
 import Header from '../components/Header'
