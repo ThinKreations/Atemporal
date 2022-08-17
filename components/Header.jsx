cd19e6a2894294db90d9ae6e1be76d26f4f681ae
ThinKreations <jnapolesm1800@alumno.ipn.mx>
2022-08-17 18:39:47 -0500

    ya?????????

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
-import logo from "../src/atempologo.png"
+
 import btn from '../src/lateral.png'
 
 export default function Header(){
@@ -10,7 +10,7 @@ export default function Header(){
 return(
 
 <div className={styles.header}>
-      <Image src={logo} width={250} height={48} />
+      <Image src={'../src/atempologo.png'} width={250} height={48} />
       <button className={styles.btnLateral}>
         <span className="material-symbols-outlined">menu</span>
       </button>
