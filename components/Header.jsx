166948286fd87951f9ac953e10d62c510beb319b
ThinKreations <jnapolesm1800@alumno.ipn.mx>
2022-08-19 23:46:54 -0500

    ya?????????

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,39 +10,39 @@ export default function Header(){
 return(
 
 <div className={styles.header}>
-      <Image src={logo} width={250} height={48} />
+      <Image src={logo} width={580} height={115} />
       <button className={styles.btnLateral}>
         <span className="material-symbols-outlined">menu</span>
       </button>
       <div className={styles.botonera}>
       
       <button className={styles.btnMenu}>
-        <font face="Inter" size='3'>
+        <font face="Work Sans" size='3'>
           <h3>Noticias</h3>
         </font>
       </button>
       <button className={styles.btnMenu}>
-        <font face="Inter" size='3'>
+        <font face="Work Sans" size='3'>
           <h3>Música</h3>
         </font>
       </button>
       <button className={styles.btnMenu}>
-        <font face="Inter" size='3'>
+        <font face="Work Sans" size='3'>
           <h3>Redes</h3>
         </font>
       </button>
       <button className={styles.btnMenu}>
-        <font face="Inter" size='3'>
+        <font face="Work Sans" size='3'>
           <h3>Nosotros</h3>
         </font>
       </button>
       <button className={styles.btnMenu}>
-        <font face="Inter" size='3'>
+        <font face="Work Sans" size='3'>
           <h3>Contacto</h3>
         </font>
       </button>
       <button className={styles.btnMenu}>
-        <font face="Inter" size='3'>
+        <font face="Work Sans" size='3'>
           <h3>Merch</h3>
         </font>
       </button>
